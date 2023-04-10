8110fedf97ee7e6e5d9b42fded6aac5e8d342cf6
Pasquale <121786472+70plus@users.noreply.github.com>
2023-04-10 09:32:30 +0200

    Updated from Publii

---
+++ media/files/tris.js
@@ -0,0 +1,67 @@
+let winPos = [
+  [1, 2, 3], [4, 5, 6], 
+  [7, 8, 9], [1, 4, 7], 
+  [2, 5, 8], [3, 6, 9], 
+  [1, 5, 9], [3, 5, 7]
+];
+
+inizioGioco("X");
+
+for (let i = 1; i <= 9; i++) {
+  // Quando si clicca su una cella
+  document.getElementById(i.toString()).addEventListener(
+    "click", 
+    function() {
+      if (this.innerHTML === "" && !gameEnded) {
+        // inserisci nella cella "X" o "O", con il colore corretto
+        this.innerHTML = playerSymbol;
+        this.classList.add(playerSymbol.toLowerCase());
+ 
+       // il giocatore di mano ha vinto?
+        checkWin(playerSymbol);        
+ 
+       // tocca all'altro giocatore
+        if (playerSymbol === "X") {playerSymbol = "O"}
+        else {playerSymbol = "X"}
+	
+	if (gameEnded === false)
+	   {document.getElementById("dialogo").innerHTML = "Clicca "+ playerSymbol + " ...";}
+      }
+    }
+  );
+}
+
+function checkWin(x) {
+  for (let i = 0; i < winPos.length; i++) {
+	if (
+	  document.getElementById(winPos[i][0]).innerHTML === x &&
+	  document.getElementById(winPos[i][1]).innerHTML === x &&
+	  document.getElementById(winPos[i][2]).innerHTML === x
+	) {
+	  document.getElementById(winPos[i][0]).classList.add("win");
+	  document.getElementById(winPos[i][1]).classList.add("win");
+	  document.getElementById(winPos[i][2]).classList.add("win"); 
+	  gameEnded = true;
+	  document.getElementById("dialogo").innerHTML = ">>> <b>"+ x + "</b> ha vinto!";
+	}
+  }
+}
+
+function inizioGioco (primo){
+	playerSymbol = primo;
+	gameEnded = false;
+	document.getElementById("dialogo").innerHTML = "Clicca "+ primo + " ...";
+}
+
+document.getElementById("reset").addEventListener(
+  "click", 
+  function() {
+    for (let i = 1; i <= 9; i++) {
+      document.getElementById(i.toString()).innerHTML = "";
+      document.getElementById(i.toString()).classList.remove("x");
+      document.getElementById(i.toString()).classList.remove("o");
+      document.getElementById(i.toString()).classList.remove("win");
+    };
+    inizioGioco(playerSymbol);
+  }
+);
