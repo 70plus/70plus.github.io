d46b399e4fbcc45dec2e2c5f92815bfdc9d96e98
Pasquale <121786472+70plus@users.noreply.github.com>
2023-07-28 21:31:22 +0200

    Updated from Publii

---
+++ media/files/bottone.js
@@ -0,0 +1,8 @@
+let form1 = document.querySelector(".cmg");
+let bottone = document.querySelector(".bottone");
+bottone.style.backgroundColor = "#1977a7";
+bottone.style.color = "white";
+bottone.style.borderRadius = "10px";
+form1.addEventListener("submit", function (e) {
+    window.open('gioca.html');
+});
