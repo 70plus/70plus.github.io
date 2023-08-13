60b5de4eb0664d05fe431a66930e859337800171
Pasquale <121786472+70plus@users.noreply.github.com>
2023-08-13 16:13:22 +0200

    Updated from Publii

---
+++ media/files/questions_0.js
@@ -0,0 +1,13 @@
+/*
+listaTest = [test1,test2,...,testN];
+test1 = [nomeTest1, descrTest, [dom1,dom2,...,domM]];
+dom1 = [testoDom,[risp1,risp2,...,rispP],[indiceRisp1,...indiceRispQ],[spiega]];
+*/
+let idNomeTest = 0;
+let idDescrTest = 1;
+let idDomande = 2;
+let idTestoDom = 0;
+let idListaRisp = 1;
+let idListaRispEsatte = 2;
+let idSpiega = 3;
+let listaTest = [];
---
+++ media/files/questions_2.js
@@ -0,0 +1,21 @@
+listaTest = listaTest.concat([
+    [`Elaborazione di testi`,
+        `... domande sui test editor ...`,
+        [`Cos'è un Test Editor`,
+            [`Un programma per elaborare testi`,
+             `Un'applicazione per tradurre testi`,
+             `Un'applicazione per preparare diapositive (o slide)`
+            ], [0],
+            `In inglese la aprola "Edit" sta per "redigere", "modificare"`
+        ],
+        [`Esempi di Test Editor`,
+            [`Chrome`,
+             `Word`,
+             `WordPress`,
+             `Notepad++`,
+             `BloccoNote`
+            ], [1,3,4],
+            `Chrome è un browser, Wordpress è la piattaforma più popolare per la gestione di siti e blog`
+        ]
+    ]
+]);
