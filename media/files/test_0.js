dedfc780b0b6d1d59b48e6422e4b89d719a03880
Pasquale <121786472+70plus@users.noreply.github.com>
2023-09-18 09:17:50 +0200

    Updated from Publii

--- media/files/test_0.js
+++ media/files/test_0.js
@@ -8,7 +8,7 @@ listaTest = listaTest.concat([
              `Word`,
              `Facebook`
             ], [0,2],
-            `"Browser è il termine generico per le applicazioni di navigazione su Internet, In inglese la parola "browser" vuol dire "navigare", ma anche "esplorare", "frugare".<br>Chrome è un particolare browser, il più diffuso sul mercato, ed è fornito dalla società Google.<br>Altri browser diffusi sono Microsoft Edge, Firefox, Brave.<br>Facebook, invece, è una delle più utilizzate applicazioni di Social Network.`
+            `"Browser" è il termine generico per le applicazioni di navigazione su Internet, e in inglese vuol dire strumento per "navigare", ma anche per "esplorare", "frugare".<br>Chrome è un particolare browser, il più diffuso sul mercato, ed è fornito dalla società Google.<br>Altri browser diffusi sono Microsoft Edge, Firefox, Brave.<br>Facebook, invece, è una delle più utilizzate applicazioni di Social Network.`
         ],
         [`Quali sono i più diffusi sistemi operativi?`,
             [`Windows 11`,
@@ -19,6 +19,39 @@ listaTest = listaTest.concat([
              `Tik Tok`
             ], [0,1,2,3],
             `Il sistema operativo è il cuore del dispositivo che utilizziamo, pc o smartphone che sia.<br>I più diffusi sistemi operativi presenti sul mercato sono: Windows 10 e 11 su pc, Android e iOS sugli smartphone.<br>Nota: le applicazioni che utilizziamo sono specifiche del sistema operativo del dispositivo, e si scaricano dai relativi Store.`
+        ],
+        [`Con quali applicazioni si accede alla posta elettronica?`,
+            [`Browser`,
+             `Applicazione specifica`,
+             `Microsoft Office`,
+             `Google`,
+             `Word`
+            ], [0,1],
+            `I servizi di posta elettronica (email) sono in genere accessibili mediante un qualunque browser, come ad esempio Chrome.<br>Esistono però anche applicazioni dedicate alle email, che vengono chiamate <b>Client</b>, come ad esempio Outlook e Thunderbird.`
+        ],
+        [`Come si protegge il pc o lo smartphone da minacce e virus?`,
+            [`Con un buon Antivirus`,
+             `Con un corretto utilizzo del dispositivo`,
+             `Non c'è difesa possibile`,
+             `Sullo smartphone non è necessario`
+            ], [0,1],
+            `Virus e hackers malevoli possono entrare nel proprio dispositivo da diverse vie: l'apertura incauta di un allegato email, l'accesso a un sito infetto, l'inserimento di una chiavetta usb trovata in giro.<br>Per difendersi, occorre innanzitutto utilizzare in modo consapevole applicazioni e accesso a Internet, e poi munirsi di un buon programma antivirus.`
+        ],
+        [`Quali di queste applicazioni sono legalmente gratuite?`,
+            [`LibreOffice`,
+             `GIMP`,
+             `Microsoft Office`,
+             `Paint`
+            ], [0,1],
+            `Il software <b>Free and Open Source</b> (FOSS) è utilizzabile gratuitamente ed è in genere molto sicuro.<br>LibreOffice, ad esempio, è un pacchetto di applicazioni per la gestione di testi, calcoli e presentazioni, simile al pacchetto a pagamento Microsoft Office.<br>GIMP è un'applicazione gratuita per l'elaborazione di immagini, simile a PhotoShop.`
+        ],
+        [`Come si possono trasferire immagini e dati tra pc e smartphone?`,
+            [`Via email`,
+             `Via cavetto usb`,
+             `Con la fionda`,
+             `Con strumenti paranormali`
+            ], [0,1],
+            `Esistono essenzialmente due metodi per il trasferimento di dati e immagini tra pc e smartphone:<br>- diretto, via cavetto, Bluetooth e simili;<br>- appoggiandosi a un'applicazione come l'email o Whatsapp sul pc.<br>Si può, ad esempio, inviare a se stessi un Whatsapp con l'immagine, e poi scaricarla sul pc accedendo al proprio Whatsapp con un browser.`
         ]
     ]
 ]);
