855693bb18d5606d6f9d8a849eb02d4f9f7806cb
Pasquale <121786472+70plus@users.noreply.github.com>
2023-09-18 08:16:50 +0200

    Updated from Publii

---
+++ media/files/test_0.js
@@ -0,0 +1,24 @@
+listaTest = listaTest.concat([
+    ['test_0', `I termini dell'informatica`,
+        `Chrome, Google, browser, sistema operativo, sicurezza, download, sappiamo destreggiarci tra questi termini?<br>Quella che segue è una panoramica sui temi oggetto del corso di informatica.<br>Nota: ciascuna domanda può prevedere una o più opzioni di risposta.`,
+        [`Quale applicazione (o programma) si utilizza per accedere a Internet?`,
+            [`Browser`,
+             `Google`,
+             `Chrome`,
+             `Word`,
+             `Facebook`
+            ], [0,2],
+            `"Browser è il termine generico per le applicazioni di navigazione su Internet, In inglese la parola "browser" vuol dire "navigare", ma anche "esplorare", "frugare".<br>Chrome è un particolare browser, il più diffuso sul mercato, ed è fornito dalla società Google.<br>Altri browser diffusi sono Microsoft Edge, Firefox, Brave.<br>Facebook, invece, è una delle più utilizzate applicazioni di Social Network.`
+        ],
+        [`Quali sono i più diffusi sistemi operativi?`,
+            [`Windows 11`,
+             `Android`,
+             `iOS`,
+             `Linux`,
+             `Instagram`,
+             `Tik Tok`
+            ], [0,1,2,3],
+            `Il sistema operativo è il cuore del dispositivo che utilizziamo, pc o smartphone che sia.<br>I più diffusi sistemi operativi presenti sul mercato sono: Windows 10 e 11 su pc, Android e iOS sugli smartphone.<br>Nota: le applicazioni che utilizziamo sono specifiche del sistema operativo del dispositivo, e si scaricano dai relativi Store.`
+        ]
+    ]
+]);
