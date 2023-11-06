93f56b8e7fb0ac755fc234d9c8b1fbe6912e7626
Pasquale <121786472+70plus@users.noreply.github.com>
2023-11-06 08:54:39 +0100

    Updated from Publii

---
+++ media/files/test_3.js
@@ -0,0 +1,39 @@
+listaTest = listaTest.concat([
+    ['test_3', `Guida all'acquisto`,
+        `Come scegliere il pc da acquistare, in base a esigenze e budget`,
+        [`Quali piattaforme si trovano più facilmente nei negozi di informatica?`,
+            [`Windows`,
+             `MacOS`,
+             `Chromebook`,
+             `Google`,
+             `Linux`
+            ], [0,1,2],
+            `I pc che si trovano più facilmente in negozio sono i <b>pc Windows</b>, i <b>Mac di Apple</b> e i <b>Chromebook di Google</b>.<br>Google non identifica un tipo di hardware ma un'azienda.<br><b>Linux</b> può essere installato su un normale pc direttamente dall'utente, mentre un pc con Linux può essere acquistato, ma generalmente online. Infatti, vista la sua bassa quota di mercato, oltretutto frammentata tra una moltitudine di <b>distribuzioni</b>, sarebbe difficile gestire scorte per un negozio fisico.`
+        ],
+        [`Ho comprato un pc Windows 11 Home S. Cosa vuol dire la <b>S</b>?`,
+            [`Configurazione di Windows con limitazioni, per aumentare la sicurezza`,
+             `Nulla`,
+             `Una versione di Windows con minore compatibilità software`,
+             `Gira su un hardware particolare`
+            ], [0],
+            `<b>Windows 11 Home S</b> è una configurazione del sistema operativo che aumenta il livello di sicurezza del pc, impedendo di installare applicazioni da fonti diverse dallo <b>store ufficiale</b>.<br>Si può facilmente passare alla configurazione standard, ma non si può tornare indietro.<br>Un utente inesperto può tenere la configurazione <b>S</b>, fino a che non gli sia assolutamente necessario installare un'applicazione non presente nello store ufficiale.`
+        ],
+        [`Quali caratteristiche devo verificare prima dell'acquisto?`,
+            [`CPU e memoria`,
+             `Disco`,
+             `Il numero e tipo di porte disponibili`,
+             `La presenza di una porta di connessione di rete`
+            ], [0,1],
+            `CPU, memoria e disco determinano la velocità di esecuzione del pc. Oggi (2023) le caratteristiche minime accettabili sono <b>6 o (meglio) 8 GBytes di memoria</b>, <b>128 GB di disco</b>, meglio se SSD. Esiste poi una varietà di tipi di processore, l'indicazione minima accettabile è quella di un processore equivalente all'<b>Intel Celeron N4500</b>.<br>Il numero di porte (USB e HDMI) è sicuramente importante, ma in genere la dotazione del pc è sufficiente per le normali applicazioni.<br>La connessione di rete WiFi è sempre presente, e non richiede una porta fisica.<br>Le uscite fisiche per cuffia e microfono tendono a sparire, a favore della connessione via Bluetooth o USB.`
+        ],
+        [`Cosa accade se compro un pc con risorse hardware limitate?`,
+            [`Il pc risulterà lento, specialmente con più applicazioni in esecuzione`,
+             `Il pc potrebbe rallentare con successive versioni del sistema operativo`,
+             `Nulla`,
+             `Il pc risulta meno sicuro`,
+             `Alcune applicazioni potrebbero non risultare più compatibili`
+            ], [0,1],
+            `Risorse hardware limitate hanno sicuramente impatto sulla velocità di esecuzione, ma nessun impatto sulla sicurezza o sulla compatibilità delle applicazioni.<br>Se possibile con il proprio budget, è preferibile acquistare un pc un po' più <em>carrozzato</em>, per reggere meglio nel tempo l'evoluzione del sistema operativo e delle applicazioni.`
+        ]
+    ]
+]);
