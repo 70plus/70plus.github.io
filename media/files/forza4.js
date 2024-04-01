d23f20e6a15a3e4b6bbf11f6ddfd3e83189df08e
Pasquale <70plus@libero.it>
2024-04-01 19:16:27 +0200

    Publii: update content

--- media/files/forza4.js
+++ media/files/forza4.js
@@ -2,6 +2,8 @@ window.onload = function() {
   const size = 6;
   const rollButton = document.getElementById('roll-button');
   const diceIcon = document.getElementById('dice-icon');
+  const hWindow = window.innerHeight;
+  const wWindow = window.innerWidth;
 
   rollButton.addEventListener('click', () => {
     diceIcon.classList.add('rolling');
@@ -13,10 +15,11 @@ window.onload = function() {
     }, 1000);
   });
 
+const tableStart = document.getElementById("pre-table").getBoundingClientRect();
 const table = document.createElement('table');
-const tablePosition = table.getBoundingClientRect();
-const cellDim = (window.innerHeight - tablePosition - '10px') / size;
-// table.style.width = '80%';
+const cellDimH = Math.trunc((hWindow - tableStart.bottom) / (size + 1));
+const cellDimW = Math.trunc(wWindow /(size + 1));
+const cellDim = Math.min(cellDimH, cellDimW);
 table.style.border = '5px solid black';
 table.style.borderCollapse = 'collapse';
 table.style.margin = '0 auto';
@@ -27,8 +30,8 @@ for (let i = 0; i < size; i++) {
     const cell = document.createElement('td');
     cell.textContent = 'text';
     cell.style.textAlign = 'center';
-    cell.style.width = '40px';
-    cell.style.height = '40px';
+    cell.style.width = cellDim + 'px';
+    cell.style.height = cellDim + 'px';
     cell.style.border = '2px solid black';
     row.appendChild(cell);
   }
