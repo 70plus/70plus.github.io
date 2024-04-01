0adc7b209b48dddec95689adb0075f7ee37609b0
Pasquale <70plus@libero.it>
2024-04-01 09:57:21 +0200

    Publii: update content

--- media/files/forza4.js
+++ media/files/forza4.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+  const size = 6;
   const rollButton = document.getElementById('roll-button');
   const diceIcon = document.getElementById('dice-icon');
 
@@ -11,4 +12,26 @@ window.onload = function() {
       diceIcon.style.backgroundImage = `url('media/files/dado-${randomNumber}.png')`;
     }, 1000);
   });
+
+const table = document.createElement('table');
+// table.style.width = '80%';
+table.style.border = '5px solid black';
+table.style.borderCollapse = 'collapse';
+table.style.margin = '0 auto'; // Add this line to center the table
+
+for (let i = 0; i < size; i++) {
+  const row = document.createElement('tr');
+  for (let j = 0; j < size; j++) {
+    const cell = document.createElement('td');
+    cell.textContent = 'text';
+    cell.style.textAlign = 'center';
+    cell.style.width = '50px';
+    cell.style.height = '50px';
+    cell.style.border = '2px solid black';
+    row.appendChild(cell);
+  }
+  table.appendChild(row);
+}
+
+document.body.appendChild(table); 
 }
