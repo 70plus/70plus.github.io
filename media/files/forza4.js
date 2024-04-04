6bdc0fa69661bd01cac1b5478b6b7a11f247d314
Pasquale <70plus@libero.it>
2024-04-04 16:43:18 +0200

    Publii: update content

--- media/files/forza4.js
+++ media/files/forza4.js
@@ -1,21 +1,36 @@
 window.onload = function() {
 //-----------------------------------------------------------
-// definizioni
+// parametri generali
   const size = 6;
-  let rollingDice = false;
-  const diceIcon = document.getElementById('dice-icon');
   const hWindow = window.innerHeight;
   const wWindow = window.innerWidth;
+// memorizzazione dello stato
+  let row = new Array(size);
+  let col = new Array(size);
+  let refRow = new Array(size);
+  let tableCont = new Array(size * size);
+  let cellOwner = new Array(size * size);
+  let avblCells = new Array(size + 1);
+// elementi del gioco
+  const diceIcon = document.getElementById('dice-icon');
   const vaiButton = document.getElementById("vai");
   const infoButton = document.getElementById("info");
   const infoBox = document.getElementById("info-box");
   const infoText = document.querySelector("#infoTesto");
   const closeButton = document.querySelector("#okB");
+  let playMsg = new Array(2);
+  playMsg[0] = document.getElementById('box-left');
+  playMsg[1] = document.getElementById('box-right');
+// giocatori
+  const playColor = new Array('#ffffff', '#3a86ff', '#ff006e');
+  let player;
+// altri parametri
   let randomNumber;
-  let tableCont = new Array(size * size);
-  let row = new Array(size);
-  let col = new Array(size);
-  let refRow = new Array(size);
+// stato del gioco
+
+  let playRunning = false;
+  let diceEnabled = false;
+  let setCellColor = false;
 //-----------------------------------------------------------
 // box di informazioni sul gioco
 infoButton.addEventListener("click", function() {
@@ -34,15 +49,24 @@ closeButton.addEventListener("click", () => {
 //-----------------------------------------------------------
 // evento "tira il dado"
   diceIcon.addEventListener('click', () => {
-    if (rollingDice == false) {
-      rollingDice = true;
+    if (diceEnabled == true) {
+      diceEnabled = false;
       diceIcon.classList.add('rolling');
       diceIcon.style.backgroundImage = `url('media/files/dado.png')`;
       setTimeout(() => {
         diceIcon.classList.remove('rolling');
         randomNumber = Math.floor(Math.random() * 6) + 1;
+        if (avblCells[randomNumber] > 0) {
+            avblCells[randomNumber] -= 1;
+            setCellColor = true;
+            playMsg[player-1].innerText = "Colora la cella";
+        } else {
+            diceEnabled = true;
+            playMsg[player - 1].innerText = "Non ci sono celle libere";
+            player = 3 - player;
+            playMsg[player - 1].innerText = "Tira il dado";
+        }
         diceIcon.style.backgroundImage = `url('media/files/dado-${randomNumber}.png')`;
-        rollingDice = false;
       }, 1000);
     }
   });
@@ -80,6 +104,7 @@ function tableGen() {
     row[i] = i;
     col[i] = i;
     refRow[i] = i;
+    avblCells[i + 1] = size;
     }
     for (i = row.length - 1; i > 0; i--) {
     	let k = Math.floor(Math.random() * (i + 1));
@@ -95,14 +120,39 @@ function tableGen() {
     }
 }
 //-----------------------------------------------------------
+// funzione colora la cella
+   function addCellsListener(i,j) {
+       document.getElementById(`${i}-${j}`).addEventListener('click', function() {
+           if (cellOwner[i + j * size] == 0 && document.getElementById(`${i}-${j}`).innerText == randomNumber)  {
+               cellOwner[i + j * size] = player;
+               document.getElementById(`${i}-${j}`).style.backgroundColor = playColor[player];
+               diceEnabled = true;
+               setCellColor = false;
+               playMsg[player - 1].innerText = "Aspetta il tuo turno";
+               player = 3 - player;
+               playMsg[player - 1].innerText = "Tira il dado";
+           }
+       });
+    }
+//-----------------------------------------------------------
 // funzione di riempimento della tabella
 vaiButton.addEventListener("click", function() {
    tableGen();
    for (let i = 0; i < size; i++) {
      for (let j = 0; j < size; j++) {
         document.getElementById(`${i}-${j}`).innerText = tableCont[i + j * size];
+        document.getElementById(`${i}-${j}`).style.backgroundColor = playColor[0];
+        cellOwner[i + j * size] = 0;
+        addCellsListener(i,j);
      }
    }
+   playRunning = true;
+   diceEnabled = true;
+   setCellColor = false;
+   player = Math.floor(Math.random() * 2) + 1;
+   console.log(player);
+   playMsg[player-1].innerText = "Tira il dado";
+   playMsg[2 - player].innerText = "Aspetta il tuo turno";
 })
 //-----------------------------------------------------------
 }
