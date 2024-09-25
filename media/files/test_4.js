c7e9ff3f4a6004c709feda55e6c52206956c7641
Pasquale <70plus@libero.it>
2024-09-25 14:13:32 +0200

    Publii: update content

--- media/files/test_4.js
+++ media/files/test_4.js
@@ -1,36 +1,3 @@
 listaTest = listaTest.concat([
-    ['test_4', `Accesso a Internet`,
-        `Internet, Web, i browser e un assaggio sulla sicurezza`,
-        [`Che differenza c'è tra Internet e Web?`,
-            [`Il Web è un particolare servizio di Internet`,
-             `Sono termini intercambiabili`,
-             `Web è un termine generico per indicare i siti di informazione`,
-             `Internet indica come le informazioni viaggiano sul Web`
-            ], [0],
-            `<b>Internet</b> indica la rete mondiale di computer, informazioni e servizi per accedere alle informazioni.<br>Il <b>Web</b> è un particolare servizio disponibile su Internet, che consente di accedere al contenuto delle pagine informative.<br>La <b>posta elettronica</b> è un altro servizio di Internet, al quale si accede con programmi appositi (es.: Outlook, Thunderbird), ma anche attraverso apposite pagine web (<b>web mail</b>).`
-        ],
-        [`Cos'è l'<b>https</b>?`,
-            [`Il protocollo sicuro di comunicazione sul web`,
-             `La modalità di accesso alle informazioni bancarie`,
-             `Una modalità di accesso che elimina la pubblicità`
-            ], [0],
-            `<b>https</b> indica il protocollo sicuro di accesso alle informazioni sul web.<br>Se l'indirizzo che si sta visitando comincia con <b>https://...</b>, allora le informazioni sono protette da crittografia nel percorso tra la sorgente e il nostro computer.<br>L'accesso al sito della propria banca, alla posta elettronica, e così via avvengono sempre via https. I siti con accesso non sicuro (<b>http://...</b>) sono sempre più rari.<br>L'https da solo, però, non garantisce la sicurezza!`
-        ],
-        [`Una buona password è sufficiente a garantire la sicurezza?`,
-            [`Non basta una buona password, occorrono anche altre cose`,
-             `Sì, perfettamente`,
-             `Basta che sia molto lunga e abbia caratteri particolari`,
-             `Se è composta da parole italiane è al sicuro dagli hackers`
-            ], [0],
-            `Una password robusta (lunga, non facilmente intuibile) è sicuramente un elemento di sicurezza indispensabile, ma non basta.<br>E bene, ad esempio, non utilizzare la stessa password su più siti, modificarla almeno una volta ogni paio d'anni e, quando possibile, affiancarla ad un'<b>autenticazione a due fattori</b>.`
-        ],
-        [`Navigare in una finestra anonima garantisce la riservatezza?`,
-            [`Le finestre anonime non garantiscono la riservatezza`,
-             `Sì, perfettamente`,
-             `Dipende dal browser`,
-             `Sì, ma solo cancellando i cookies regolarmente`
-            ], [0],
-            `La finestra di navigazione anonima riduce le informazioni di navigazione lasciate sul computer e raccolte dai siti visitati, ma non garantisce per questo la riservatezza. In particolare, il proprio indirizzo di rete (<b>indirizzo ip</b>) è comunque visibile.<br>Particolari browser, come ad esempio <b>Brave</b> riducono in modo significativo le informazioni riguardanti il nosto pc raccolte dai siti visitati.`
-        ]
-    ]
+
 ]);
--- media/files/test_5.js
+++ media/files/test_5.js
@@ -1,37 +1,3 @@
 listaTest = listaTest.concat([
-    ['test_5', `Dispositivi mobili`,
-        `Classificazione dei dispositivi mobili, gli Store, le App più utilizzate`,
-        [`In base a che criteri scegliere una App?`,
-            [`Reale utilità`,
-             `Recensioni e Privacy Policy`,
-             `Bisogna provarla per farsi una prima idea`,
-             `Seguire il parere degli amici`
-            ], [0,1],
-            `È bene essere selettivi nella scelta delle app da installare sul proprio dispositivo mobile, specialmente se utilizziamo <b>anche app bancarie</b>.<br>Chiedersi sempre se l'app che stiamo per installare sia realmente necessaria, e verificare il punteggio dell'App nello store (bene che sia superiore a 4), verificare se <b>le recensioni recenti segnalino problemi</b>.<br>Infine verificare anche i termini della Privacy Policy.`
-        ],
-        [`Quante app di posta elettronica devo avere?`,
-            [`Per ciascun servizio è bene avere l'app del fornitore del servizio`,
-             `Ne uso una per tutte`,
-             `Non importa la sicurezza, le mie caselle non hanno nulla di importante`
-            ], [0],
-            `All'applicazione che utilizziamo per gestire la posta elettronica affidiamo login e password delle caselle. È più sicuro che queste informazioni <b>rimangano visibili solo al fornitore del servizio</b> e non a terze parti.`
-        ],
-        [`Cosa pubblicare su un social, come ad esempi Facebook?`,
-            [`Limitare le informazioni personali riguardanti familiari minori, località in cui si è al momento, e simili`,
-             `Si può scrivere di tutto, tanto si può eventualmente cancellare`,
-             `A nessuno può importare cosa scrivo, sono cose banali`,
-             `Posso scrivere quello che voglio, tanto ho pochi amici / follower`
-            ], [0],
-            `È bene ricordare che quello che si rende pubblico su Internet <b>esce dal nostro controllo</b>: non è sufficiente cancellarlo successivamente, potrà rimanere visibile a chiunque nel mondo, potrà addirittura essere utilizzato per <b>creare un mio profilo <em>fake</em></b>, con cui commettere crimini informatici.<br>Meglio evitare di pubblicare troppe informazioni.`
-        ],
-        [`Di quale e-commerce fidarsi?`,
-            [`Fidarsi di piattaforme solide, come Amazon, Alibaba, IBS, ...`,
-             `Oltre alla piattaforma occorre verificare anche l'affidabilità dei venditori che operano su quella piattaforma`,
-             `Non compro niente online, non è sicuro`,
-             `Non compro niente online, preferisco privilegiare il commercio locale`,
-             `Mi fido solo di Amazon`
-            ], [0,1],
-            `La piattaforma di e-commerce, come ad esempio Amazon, conosce i nostri indirizzi di recapito, i nostri metodi di pagamento, le nostre preferenze in termini di prodotti. Sono tutte informazioni estremamente sensibili.<br>Fidarsi quindi solo di piattaforme solide, per ridurre al minimo il rischio di <b>diffusione delle nostre informazioni</b>.<br>Inoltre la piattaforma deve anche offrire garanzie sui resi e sulle mancate consegne.<br>Verificare bene, prima dell'acquisto, anche il venditore che ci sta fornendo il prodotto: che recensioni ha? quante?`
-        ]
-    ]
+
 ]);
