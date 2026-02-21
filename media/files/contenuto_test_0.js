5f28a83aa331cfb6320e03f9b045b1afea61f760
Pasquale <70plus@libero.it>
2026-02-21 17:08:46 +0100

    Publii: update content

---
+++ media/files/contenuto_test_0.js
@@ -0,0 +1,25 @@
+const listaTest = [
+    ['Italiano','Inserisci <b><em>a</em></b>, oppure <b><em>ha</em><b>','mono',[
+    "Vado §a§ casa.",
+    "La mamma §ha§ acceso il tv.",
+    "La nonna §ha§ i capelli ricci.",
+    "Mando un saluto §a§ tutti.",
+    "Il nonno §ha§ rotto."
+    ]],
+    ['Matematica','Operazioni!','mono',[
+    "3 x 8 fa §24§.",
+    "6 x 4 fa §24§."
+    ]],
+    ['Italiano','2-Inserisci <b><em>a</em></b>, oppure <b><em>ha</em><b>','mono',[
+    "Vado §a§ casa.",
+    "La mamma §ha§ acceso il tv.",
+    "La nonna §ha§ i capelli ricci.",
+    "Mando un saluto §a§ tutti.",
+    "Il nonno §ha§ rotto."
+    ]],
+    ['Matematica','2-Operazioni!','mono',[
+    "3 x 8 fa §24§.",
+    "6 x 4 fa §24§."
+    ]]
+    ];
+    
