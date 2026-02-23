91e1d6f06e0ad5788294e64ca145d3136f662f13
Pasquale <70plus@libero.it>
2026-02-23 16:23:08 +0100

    Publii: update content

--- media/files/testoquiz.js
+++ media/files/testoquiz.js
@@ -10,39 +10,67 @@ La nonna §ha§ i capelli ricci.
 Mando un saluto §a§ tutti.
 Il nonno §ha§ rotto.
 Il topo §ha§ fatto ritorno §a§ casa.
+Il gatto §ha§ mangiato tutto.
+Marco va §a§ scuola a piedi.
+La mamma §ha§ una borsa nera.
+Ho regalato un disegno §a§ mia nonna.
+Sofia §/ha§ mal di pancia.
+Luca §ha§ visto un film bello.
+Vado §a§ giocare nel parco.
+Il cane §ha§ paura del temporale.
+Scrivi §a§ matita, per favore.
+Sara §ha§ guardato un film §a§ casa.
+
+---   
+Italiano
+Inserisci <b><em>e</em></b>, oppure <b><em>è</em><b>
+mono
+Il cane §è§ marrone §e§ mangia un osso.
+La mamma §è§ andata al supermercato §e§ ha comprato la frutta.
+Il libro §è§ sul tavolo §e§ parla di pirati.
+La prugna §è§ un frutto dolce §e§ cresce sull'albero.
+Il cielo oggi §è§ azzurro §e§ sereno.
+La mia amica §è§ molto simpatica.
+Ho comprato quaderni §e§ matite.
+Il sole §è§ caldo §e§ luminoso.
+Il gatto §è§ sul tetto.
+Il cane §e§ il gatto giocano.
+La penna §è§ rossa §e§ blu.
 
 --- 
 Matematica
 Tabelline!
 mono
-5 × 6 fa §30§.
-3 × 3 fa §9§.
+6 × 5 fa §30§.
+7 × 1 fa §7§.
+2 × 3 fa §6§.
+3 × 6 fa §18§.
 4 × 2 fa §8§.
-3 × 4 fa §12§.
-7 × 3 fa §21§.
+4 × 6 fa §24§.
+3 × 7 fa §21§.
+5 × 10 fa §50§.
+2 × 5 fa §10§.
+5 × 1 fa §5§.
 5 × 6 fa §30§.
-3 × 7 fa §21§.
-3 × 7 fa §21§.
+3 × 8 fa §24§.
+7 × 3 fa §21§.
+2 × 10 fa §20§.
+7 × 6 fa §42§.
+6 × 7 fa §42§.
+2 × 8 fa §16§.
+4 × 4 fa §16§.
+3 × 3 fa §9§.
+3 × 4 fa §12§.
 6 × 2 fa §12§.
 3 × 2 fa §6§.
 7 × 7 fa §49§.
 4 × 9 fa §36§.
 4 × 8 fa §32§.
 7 × 4 fa §28§.
-7 × 8 fa §56§.
-6 × 2 fa §12§.
 2 × 4 fa §8§.
 7 × 2 fa §14§.
 6 × 8 fa §48§.
 5 × 4 fa §20§.
----   
-Italiano
-Inserisci <b><em>e</em></b>, oppure <b><em>è</em><b>
-mono
-Oggi §e§ domani sono a casa.
-La mamma §è§andata al lavoro.
-La nonna §è§ uscita §e§ verrà da me.
-Siamo tutti §e§ tre al mare.
 
 --- 
 Scienze
@@ -67,4 +95,5 @@ mono
 7 + 14 fa §21§.
 13 + 9 fa §22§.
 2 + 19 fa §21§.
+
 `;
