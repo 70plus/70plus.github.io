e5e15f2997e403490725f86cbbefe7edc9f23e34
Pasquale <70plus@libero.it>
2026-02-23 07:48:52 +0100

    Publii: update content

---
+++ media/files/testoquiz.js
@@ -0,0 +1,63 @@
+const DATI_TEST = `
+---
+Italiano
+Inserisci <b><em>a</em></b>, oppure <b><em>ha</em><b>
+mono
+Vado §a§ casa.
+Mi aiuti §a§ sollevare questa scatola?
+La mamma §ha§ acceso il tv.
+La nonna §ha§ i capelli ricci.
+Mando un saluto §a§ tutti.
+Il nonno §ha§ rotto.
+Il topo §ha§ fatto ritorno §a§ casa.
+
+--- 
+Matematica
+Tabelline!
+mono
+3 x 8 fa §24§.
+6 x 4 fa §24§.
+5 x 4 fa §20§
+4 x 9 fa §36§
+3 x 2 fa §6§
+8 x 2 fa §16§
+4 x 5 fa §20§
+6 x 6 fa §36§
+7 x 3 fa §21§
+3 x 9 fa §27§
+2 x 6 fa §12§
+2 x 8 fa §16§
+6 x 8 fa §48§
+---   
+Italiano
+Inserisci <b><em>e</em></b>, oppure <b><em>è</em><b>
+mono
+Oggi §e§ domani sono a casa.
+La mamma §è§andata al lavoro.
+La nonna §è§ uscita §e§ verrà da me.
+Siamo tutti §e§ tre al mare.
+
+--- 
+Scienze
+Gli esseri viventi
+bi
+Il §ciclo vitale§ è la caratteristica principale che definisce un essere vivente, e comprende: nascita, crescita, riproduzione e morte.
+La §respirazione§ è lo scambio di gas degli esseri viventi con l'ambiente esterno.
+Le §funzioni vitali§ degli esseri viventi sono: respirazione, nutrizione, movimento, riproduzione e reazione agli stimoli.
+La §reazione agli stimoli§ consente agli esseri viventi di sopravvivere, accorgendosi di eventuali pericoli.
+
+---    
+Matematica
+Calcolo mentale!
+mono
+8 + 5 fa §13§.
+9 + 4 fa §13§.
+3 + 9 fa §12§.
+7 + 7 fa §14§.
+19 + 3 fa §22§.
+8 + 6 fa §14§.
+11 + 8 fa §19§.
+7 + 14 fa §21§.
+13 + 9 fa §22§.
+2 + 19 fa §21§.
+`;
