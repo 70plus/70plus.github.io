51ab3fa1b8342223bf133d5747b2ddc384a519ce
Pasquale <70plus@libero.it>
2026-02-27 13:38:12 +0100

    Publii: update content

--- media/files/testoquiz.js
+++ media/files/testoquiz.js
@@ -1,8 +1,41 @@
 const DATI_TEST = `
 ---
 Italiano
+Parole difficili!
+mono
+alfa
+<span style="font-size: 400%;">&#x1f528;</span> §martello§
+<span style="font-size: 400%;">&#x1f407;</span> §coniglio§
+<span style="font-size: 400%;">&#x1f3af;</span> §bersaglio§
+<span style="font-size: 400%;">&#x1f392;</span> §zaino§
+<span style="font-size: 400%;">&#x1f550;</span> §orologio§
+<span style="font-size: 400%;">&#x1f4a7;</span> §goccia§
+<span style="font-size: 400%;">&#x1f941;</span> §tamburo§
+<span style="font-size: 400%;">&#x1f577;</span> §ragno§
+<span style="font-size: 400%;">&#x1f578;</span> §ragnatela§
+<span style="font-size: 400%;">&#x2602;</span> §ombrello§
+<span style="font-size: 400%;">&#x1f485;</span> §unghie§
+<span style="font-size: 400%;">&#x1f484;</span> §rossetto§
+<span style="font-size: 400%;">&#x1f40d;</span> §serpente§
+<span style="font-size: 400%;">&#x1f9a2;</span> §cigno§
+<span style="font-size: 400%;">&#x1f48d;</span> §anello§
+<span style="font-size: 400%;">&#x1f992;</span> §giraffa§
+<span style="font-size: 400%;">&#x1f444;</span> §labbra§
+<span style="font-size: 400%;">&#x1f3f0;</span> §castello§
+<span style="font-size: 400%;">&#x1fa9e;</span> §specchio§
+<span style="font-size: 400%;">&#x1f5e1;</span> §pugnale§
+<span style="font-size: 400%;">&#x1f9fd;</span> §spugna§
+<span style="font-size: 400%;">&#x1f37e;</span> §bottiglia§
+<span style="font-size: 400%;">&#x1f341;</span> §foglia§
+<span style="font-size: 400%;">&#x1f9c4;</span> §aglio§
+<span style="font-size: 400%;">&#x2606;</span> §stella§
+<span style="font-size: 400%;">&#x2764;</span> §cuore§
+<span style="font-size: 400%;">&#x2702;</span> §forbici§
+---
+Italiano
 Inserisci <b><em>a</em></b>, oppure <b><em>ha</em><b>
 mono
+alfa
 Vado §a§ casa.
 Mi aiuti §a§ sollevare questa scatola?
 La mamma §ha§ acceso il tv.
@@ -25,6 +58,7 @@ Sara §ha§ guardato un film §a§ casa.
 Italiano
 Inserisci <b><em>e</em></b>, oppure <b><em>è</em><b>
 mono
+alfa
 Il cane §è§ marrone §e§ mangia un osso.
 La mamma §è§ andata al supermercato §e§ ha comprato la frutta.
 Il libro §è§ sul tavolo §e§ parla di pirati.
@@ -41,6 +75,7 @@ La penna §è§ rossa §e§ blu.
 Matematica
 Tabelline!
 mono
+num
 6 × 5 fa §30§.
 7 × 1 fa §7§.
 2 × 3 fa §6§.
@@ -56,7 +91,7 @@ mono
 7 × 3 fa §21§.
 2 × 10 fa §20§.
 7 × 6 fa §42§.
-6 × 7 fa §42§.
+6 × 9 fa §54§.
 2 × 8 fa §16§.
 4 × 4 fa §16§.
 3 × 3 fa §9§.
@@ -71,11 +106,13 @@ mono
 7 × 2 fa §14§.
 6 × 8 fa §48§.
 5 × 4 fa §20§.
-
+5 × 8 fa §40§.
+5 × 9 fa §45§.
 --- 
 Scienze
 Gli esseri viventi
 bi
+alfa
 Il §ciclo vitale§ è la caratteristica principale che definisce un essere vivente, e comprende: nascita, crescita, riproduzione e morte.
 La §respirazione§ è lo scambio di gas degli esseri viventi con l'ambiente esterno.
 Le §funzioni vitali§ degli esseri viventi sono: respirazione, nutrizione, movimento, riproduzione e reazione agli stimoli.
@@ -85,6 +122,7 @@ La §reazione agli stimoli§ consente agli esseri viventi di sopravvivere, accor
 Matematica
 Calcolo mentale!
 mono
+num
 8 + 5 fa §13§.
 9 + 4 fa §13§.
 3 + 9 fa §12§.
